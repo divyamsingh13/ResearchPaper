da6989adc1549cd06a2cc9fc334b93c80b67cfed
divyamsingh13 <divyamsingh13@gmail.com>
2019-01-11 16:44:35 +0530

    initial

--- download_pdf.py
+++ download_pdf.py
@@ -2,55 +2,68 @@ import requests
 import bs4
 import os
 '''retrival of paper id and title '''
-
-def search_by_keyword(keyword1,search='Search',count=0,d={}):
-    data={"keyword":keyword1,"submit":search}
-
-    response=requests.post(url="https://www.ijsr.net/search_index_results.php",data=data)
-
-    soup=bs4.BeautifulSoup(response.text)
-    soup=soup.find("div",{"id":"middle"})
-    soup=soup.find_all("table")
-    soup=soup[1:51]
+class download_pdf(object):
 
 
-    for table in soup:
-        row=table.find("tr")
-        col=row.find_all("td")
-        title=col[0].a.font.text
-        version=col[0].a['href']
-        country=col[1].em.font.text
-        paper_id=col[2].find("input",attrs={"name":"paper_id"})['value']
-        if(paper_id[0].isdigit()):
-            continue
+    def __init__(self,keyword,count=50):
+        self.keyword=keyword
+        self.count=count
+
+    def search_by_keyword(self,search='Search',d={},count1=0):
+        keyword1=self.keyword
+
+        data={"keyword":keyword1,"submit":search}
+
+        response=requests.post(url="https://www.ijsr.net/search_index_results.php",data=data)
+
+        soup=bs4.BeautifulSoup(response.text)
+        soup=soup.find("div",{"id":"middle"})
+        soup=soup.find_all("table")
+        soup=soup[1:51]
+
+
+        for table in soup:
+            row=table.find("tr")
+            col=row.find_all("td")
+            title=col[0].a.font.text
+            version=col[0].a['href']
+            country=col[1].em.font.text
+            paper_id=col[2].find("input",attrs={"name":"paper_id"})['value']
+            if(paper_id[0].isdigit()):
+                continue
+            else:
+                count1+=1
+            d[paper_id]=[title,country,version]
+        if(count1<self.count):
+            return self.search_by_keyword(search="Search Again",count1=count1,d=d)
         else:
-            count+=1
-        d[paper_id]=[title,country,version]
-    if(count<200):
-        return search_by_keyword(keyword1,search="Search Again",count=count,d=d)
-    return d
+            print(count1)
+            print(keyword1)
+            return d
+    def download(self,dict):
+        keyword=self.keyword
+        print("hfcgf",len(dict.keys()))
+        for i in dict.keys():
 
-'''retrival of downloaded papers'''
-def download(dict,keyword):
-    for i in dict.keys():
+            version=dict[i][2]
+            v=version.rfind("/")
+            v=version[:v+1]+i+".pdf"
+            response=requests.get(url=v)
+            pdf_name = dict[i][0]+".pdf"
+            pdf_path = os.path.join("pdfs",keyword,pdf_name)
+            if not os.path.exists(pdf_path):
 
-        version=dict[i][2]
-        v=version.rfind("/")
-        v=version[:v+1]+i+".pdf"
-        response=requests.get(url=v)
-        pdf_name = dict[i][0]+".pdf"
-        pdf_path = os.path.join("pdfs",keyword,pdf_name)
-        if not os.path.exists(pdf_path):
-
-            if not os.path.exists(os.path.dirname(pdf_path)):
-                os.makedirs(os.path.dirname(pdf_path))
-            pdf_file = open(pdf_path, "wb")
-            pdf_file.write(response.content)
-            pdf_file.close()
+                if not os.path.exists(os.path.dirname(pdf_path)):
+                    os.makedirs(os.path.dirname(pdf_path))
+                pdf_file = open(pdf_path, "wb")
+                pdf_file.write(response.content)
+                pdf_file.close()
 
 
 keywords=['heart','artifical intelligence']
 for i in keywords:
-    d=search_by_keyword(i)
-    print(len(d))
-    download(d,i)
+    d=download_pdf(i)
+    d1=d.search_by_keyword()
+
+
+    d.download(d1)
